d613cf50ec4709a3fdc4b9c6387a24a775c7ea6a
jmavis <jaredmavis@outlook.com>
2014-10-31 22:46:32 -0700

    Render posts with html inside. And fixing warning about content not being set.

--- data/posts.js
+++ data/posts.js
@@ -2,5 +2,5 @@ window.posts = [{
 	title: "Website update in progress",
 	date: "August 10 2014",
 	author: "Jared",
-	content: "Hello, welcome to my site. I am currently in the process of updating everything to be based around ember.js. Feel free to look around.",
+	content: "Hello, welcome to my site.</br> I am currently in the process of updating everything to be based around ember.js. Feel free to look around.",
 }]
